8d5c7a666bb7341b173a97e2fe165ec65eb3d5b6
Ayush Kumar <ayushkumar6064@gmail.com>
2025-07-09 12:27:45 +0530

    added controllers auth user routes

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,6 +3,23 @@ import { ApiError } from "../utils/ApiError.js"
 import { User } from "../models/user.models.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken"
+
+const generateAccessAndRefreshTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        user.save({ validateBeforeSave: false })
+
+        return { accessToken, refreshToken }
+
+    } catch (error) {
+        throw new ApiError(500, "Something wwent wrong while generating refresh and access token")
+    }
+}
 const registerUser = asyncHandler(async (req, res) => {
 
     //  step-1  get user details from frontend 
@@ -32,7 +49,7 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "All fields are compulsory and required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -66,17 +83,139 @@ const registerUser = asyncHandler(async (req, res) => {
     const createdUser = await User.findById(user._id).select(
         "-password -refreshToken"
     )
-if (!createdUser) {
-    throw new ApiError(500, "Something went wrong while registering the user")
-}
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering the user")
+    }
 
-return res.status(201).json(
-    new ApiResponse(200, createdUser, "User Registered Successfully")
-)
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User Registered Successfully")
+    )
 })
 
+
+const loginUSer = asyncHandler(async (req, res) => {
+
+    // request body se data le aao i.e. req body -> data
+    // username or email 
+    // find the user 
+    // password check 
+    // access and refresh token
+    // send cookie 
+
+    const { email, password, username } = req.body
+    if (!username && !email) {
+        throw new ApiError(400, "username or email is required")
+    }
+
+    const user = await User.findOne({
+        $or: [{ username }, { email }]
+    })
+    if (!user) {
+        throw new ApiError(404, "USer does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid user credentials")
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id)
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user: loggedInUser, accessToken, refreshToken
+                },
+                "User logged In Successfully"
+            )
+        )
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+   await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(new ApiResponse(200, {} , "User Logged Out"))
+})
+
+const refreshAccessToken = asyncHandler(async(req,res) => {
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+
+    if (incomingRefreshToken) {
+        throw new ApiError(401, "Unauthorized request")
+    }
+
+  try {
+     const decodedToken =  jwt.verify(
+          incomingRefreshToken,
+          process.env.REFRESH_TOKEN_SECRET
+      )
+  
+      const user = await User.findById(decodedToken?._id)
+  
+       if (!user) {
+          throw new ApiError(401, "Invalid Refresh Token`")
+      }
+  
+      if (incomingRefreshToken !== user?.refreshToken){
+          throw new ApiError(401, "Refresh token is expired or used")
+      }
+  
+      const options = {
+          httpOnly: true,
+          secure: true
+      }
+     const {accessToken, newRefreshToken}= await generateAccessAndRefreshTokens(user._id)
+  
+     return res
+     .status(200)
+     .cookies("accessToken", accessToken, options)
+     .cookies("refreshToken", newRefreshToken, options)
+     .json(
+      new ApiResponse(
+          200,
+          { accessToken, refreshToken: newRefreshToken} ,
+              "Access token refreshed"
+      )
+     )
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid refresh token")
+  }
+})
 export {
     registerUser,
+    loginUSer,
+    logoutUser,
+    refreshAccessToken
 }
 
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controllers.js";
+import { loginUSer, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
 
 router.route("/register").post(
@@ -8,7 +9,7 @@ router.route("/register").post(
         {
             name: "avatar",
             maxCount: 1
-        },
+        },                  
         {
             name: "coverImage",
             maxCount: 1
@@ -16,8 +17,12 @@ router.route("/register").post(
     ]),
     registerUser)
 
+router.route("/login").post(loginUSer)
 
+//  secured routes
 
+router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router
 
